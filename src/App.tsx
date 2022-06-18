7e5f1fa2745e7fe9fbaaa19b76c40de507c3c234
lxxonx <leeonechang92@gmail.com>
2022-06-18 19:45:08 +0900

    feat: css/animation 관련 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import "./styles/App.css";
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Lottie from "../../components/Lottie";
+
+function AboutPage() {
+  return (
+    <div>
+      <Lottie src="https://assets6.lottiefiles.com/packages/lf20_cmsczshd.json" />
+    </div>
+  );
+}
+
+export default AboutPage;
